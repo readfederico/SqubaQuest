9a9cea870d01bb21f3c18108098c0f8bbc8aa28b
readfederico <41490821+readfederico@users.noreply.github.com>
2019-02-26 19:53:54 -0500

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 250.0f;
+    private Rigidbody2D _body;
+    private Animator _anim;
+    private BoxCollider2D _box;
+    void Start()
+    {
+        _body = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+        _box = GetComponent<BoxCollider2D>();
+
+    }
+
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float deltaY = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        Vector2 movement = new Vector2(deltaX, deltaY);
+        _body.velocity = movement;
+
+    }
+
+}
